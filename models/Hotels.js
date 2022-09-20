5bb05c10bb2d9de4412517e8b8c5f34a83129c73
phantware <phantmoney2011@gmail.com>
2022-09-20 14:49:44 +0100

    Hotels model created

--- models/Hotels.js
+++ models/Hotels.js
@@ -1,5 +1,44 @@
 import mongoose from 'mongoose'
+const { Schema } = 'mongoose'
 
-const hotelSchema = mongoose.Schema({})
+const HotelSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  distance: {
+    type: String,
+    required: true,
+  },
+  photos: {
+    type: [String],
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5,
+  },
+  rooms: {
+    type: [String],
+    required: true,
+  },
+})
 
-export default hotelSchema
+export default HotelSchema
