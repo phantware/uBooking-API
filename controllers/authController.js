00b16808dc4b88dca5c36ad51a1d7c3e73a445c3
phantware <phantmoney2011@gmail.com>
2022-09-21 17:32:10 +0100

    register route created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,15 @@
-export const register = async (req, res, next) => {}
+import User from '../models/User.js'
+export const register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body
+    const newUser = new User({
+      username,
+      email,
+      password,
+    })
+    await newUser.save()
+    return res.status(201).json('User has been created successfully.')
+  } catch (err) {
+    next(err)
+  }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,8 @@
 import express from 'express'
+import { register } from '../controllers/authController.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  return res.status(200).json({ msg: 'This is our auth path' })
-})
+router.post('/register', register)
 
 export default router
