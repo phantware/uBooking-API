a42da9b2ecf2ff1619fea36b0297aadd287f7a23
phantware <phantmoney2011@gmail.com>
2022-09-21 17:50:18 +0100

    bcryptjs used for password encryption

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,15 @@
 import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
 export const register = async (req, res, next) => {
   try {
     const { username, email, password } = req.body
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+
     const newUser = new User({
       username,
       email,
-      password,
+      password: hash,
     })
     await newUser.save()
     return res.status(201).json('User has been created successfully.')
