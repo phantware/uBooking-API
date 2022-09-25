4b7297d9cf58a7f563d21401d78f161f3d02908b
phantware <phantmoney2011@gmail.com>
2022-09-25 21:35:12 +0100

    verifyToken created to verify the user token

---
+++ utils/verifyToken.js
@@ -0,0 +1,16 @@
+import { createError } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token
+  if (!token) {
+    return next(createError(401, 'You are not authenticated'))
+  }
+
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) return next(createError(403, 'Token not valid!'))
+
+    req.user = user
+    next()
+  })
+}
