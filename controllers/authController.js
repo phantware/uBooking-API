5137a0e1a868fa2d9d65da4b10a58ea3e6b01b86
phantware <phantmoney2011@gmail.com>
2022-09-25 20:14:30 +0100

    JWT Secret added to env

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import User from '../models/User.js'
 import bcrypt from 'bcryptjs'
 import { createError } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 export const register = async (req, res, next) => {
   try {
     const { username, email, password } = req.body
@@ -31,6 +32,11 @@ export const login = async (req, res, next) => {
     )
     if (!isPasswordCorrect)
       return next(createError(400, 'Wrong Username or Password'))
+
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    )
 
     const { password, isAdmin, ...otherDetails } = user._doc
     return res.status(200).json({ ...otherDetails })
