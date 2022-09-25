84319bb68541f82c99e6dc867680c1a56cbd9ea5
phantware <phantmoney2011@gmail.com>
2022-09-25 20:23:59 +0100

    cooki and token added to login path

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,7 +39,10 @@ export const login = async (req, res, next) => {
     )
 
     const { password, isAdmin, ...otherDetails } = user._doc
-    return res.status(200).json({ ...otherDetails })
+    return res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json({ ...otherDetails })
   } catch (err) {
     next(err)
   }
