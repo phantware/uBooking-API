c4fe36d626270abd1dc07e0f6c5962ea26fc16c2
phantware <phantmoney2011@gmail.com>
2022-10-25 18:29:13 +0100

    authController modified

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,36 +2,34 @@ import User from '../models/User.js'
 import bcrypt from 'bcryptjs'
 import { createError } from '../utils/error.js'
 import jwt from 'jsonwebtoken'
+
 export const register = async (req, res, next) => {
   try {
-    const { username, email, password } = req.body
     const salt = bcrypt.genSaltSync(10)
-    const hash = bcrypt.hashSync(password, salt)
+    const hash = bcrypt.hashSync(req.body.password, salt)
 
     const newUser = new User({
-      username,
-      email,
+      ...req.body,
       password: hash,
     })
+
     await newUser.save()
-    return res.status(201).json('User has been created successfully.')
+    res.status(200).send('User has been created.')
   } catch (err) {
     next(err)
   }
 }
-
 export const login = async (req, res, next) => {
-  const { username } = req.body
   try {
-    const user = await User.findOne({ username })
-    if (!user) return next(createError(404, 'User not found'))
+    const user = await User.findOne({ username: req.body.username })
+    if (!user) return next(createError(404, 'User not found!'))
 
     const isPasswordCorrect = await bcrypt.compare(
       req.body.password,
       user.password
     )
     if (!isPasswordCorrect)
-      return next(createError(400, 'Wrong Username or Password'))
+      return next(createError(400, 'Wrong password or username!'))
 
     const token = jwt.sign(
       { id: user._id, isAdmin: user.isAdmin },
@@ -39,10 +37,12 @@ export const login = async (req, res, next) => {
     )
 
     const { password, isAdmin, ...otherDetails } = user._doc
-    return res
-      .cookie('access_token', token, { httpOnly: true })
+    res
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
       .status(200)
-      .json({ ...otherDetails })
+      .json({ details: { ...otherDetails }, isAdmin })
   } catch (err) {
     next(err)
   }
