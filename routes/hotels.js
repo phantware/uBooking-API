238718e5d42acdaaefec11caa3db3e30ef559682
phantware <phantmoney2011@gmail.com>
2022-10-26 08:08:35 +0100

    new route added

--- routes/hotels.js
+++ routes/hotels.js
@@ -7,6 +7,7 @@ import {
   updateHotel,
   countByCity,
   countByType,
+  getHotelRooms,
 } from '../controllers/hotelController.js'
 import { createError } from '../utils/error.js'
 import { verifyAdmin } from '../utils/verifyToken.js'
@@ -29,5 +30,6 @@ router.get('/find/:id', getHotel)
 router.get('/', getHotels)
 router.get('/countByCity', countByCity)
 router.get('/countByType', countByType)
+router.get('/room/:id', getHotelRooms)
 
 export default router
