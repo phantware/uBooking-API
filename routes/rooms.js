f7abc5b78ca18a416ef598888bde71c0e4d1b599
phantware <phantmoney2011@gmail.com>
2022-10-26 09:20:05 +0100

    updateRoomAvailability route added

--- routes/rooms.js
+++ routes/rooms.js
@@ -5,6 +5,7 @@ import {
   getAllRooms,
   getRoom,
   updateRoom,
+  updateRoomAvailability,
 } from '../controllers/roomController.js'
 import { createError } from '../utils/error.js'
 import { verifyAdmin } from '../utils/verifyToken.js'
@@ -15,6 +16,7 @@ router.post('/:hotelid', verifyAdmin, createRoom)
 
 //UPDATE
 router.put('/:id', verifyAdmin, updateRoom)
+router.put('/availability/:id', updateRoomAvailability)
 
 //DELETE
 router.delete('/:id/:hotelid', verifyAdmin, deleteRoom)
